4db589d8e1d56c2f2a7a4f5ba94936e17e8e20a6
Yan Lukavii <mr.winston93@mail.ru>
2024-03-12 12:28:09 +0300

    hw17 depends_on, app and bd naming + jpa tests

---
+++ hw17-docker/src/test/java/ru/otus/hw/repositories/JpaCommentRepositoryTest.java
@@ -0,0 +1,158 @@
+package ru.otus.hw.repositories;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.test.context.TestPropertySource;
+import ru.otus.hw.models.Author;
+import ru.otus.hw.models.Book;
+import ru.otus.hw.models.Comment;
+import ru.otus.hw.models.Genre;
+
+import java.util.List;
+import java.util.stream.IntStream;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@DisplayName("Репозиторий на основе Jpa для работы с комментариями ")
+@DataJpaTest
+@TestPropertySource(locations = "classpath:test-application.yml")
+public class JpaCommentRepositoryTest {
+    @Autowired
+    private CommentRepository commentRepository;
+
+    @Autowired
+    private TestEntityManager em;
+
+    private List<Author> dbAuthors;
+
+    private List<Genre> dbGenres;
+
+    private List<Book> dbBooks;
+
+    private List<Comment> dbComments;
+
+    @BeforeEach
+    void setUp() {
+        dbAuthors = getDbAuthors();
+        dbGenres = getDbGenres();
+        dbBooks = getDbBooks(dbAuthors, dbGenres);
+        dbComments = getDbComments(dbBooks);
+    }
+
+    @DisplayName("должен загружать комментарий по id")
+    @ParameterizedTest
+    @MethodSource("getDbComments")
+    void shouldReturnCorrectCommentById(Comment expectedComment) {
+
+        var actualComment = commentRepository.findById(expectedComment.getId());
+
+        assertThat(actualComment)
+                .get()
+                .usingRecursiveComparison()
+                .ignoringFields("book")
+                .isEqualTo(expectedComment);
+    }
+
+    @DisplayName("должен загружать список всех комментариев по книге")
+    @Test
+    void shouldReturnCorrectCommentsListByBook() {
+        var actualComments = commentRepository.findByBook(dbBooks.get(0));
+        var expectedComments = dbComments.stream()
+                .filter(comment -> comment.getBook().getId() == 1L)
+                .toList();
+
+        assertThat(actualComments)
+                .usingRecursiveComparison()
+                .ignoringFields("book")
+                .isEqualTo(expectedComments);
+    }
+
+    @DisplayName("должен сохранять новый комментарий")
+    @Test
+    void shouldSaveNewComment() {
+
+        var expectedComment = new Comment(0, "CommentTitle_10500", dbBooks.get(0));
+        var returnedComment = commentRepository.save(expectedComment);
+
+        assertThat(returnedComment).isNotNull()
+                .matches(comment -> comment.getId() > 0)
+                .usingRecursiveComparison()
+                .ignoringExpectedNullFields()
+                .isEqualTo(expectedComment);
+
+        assertThat(commentRepository.findById(returnedComment.getId()))
+                .isPresent()
+                .get()
+                .isEqualTo(returnedComment);
+    }
+
+    @DisplayName("должен сохранять измененный комментарий")
+    @Test
+    void shouldSaveUpdatedComment() {
+        var expectedComment = new Comment(1L, "CommentTitle_10500", dbBooks.get(2));
+
+        assertThat(commentRepository.findById(expectedComment.getId()))
+                .isPresent()
+                .get()
+                .isNotEqualTo(expectedComment);
+
+        var returnedComment = commentRepository.save(expectedComment);
+        assertThat(returnedComment).isNotNull()
+                .matches(comment -> comment.getId() > 0)
+                .usingRecursiveComparison().ignoringExpectedNullFields().isEqualTo(expectedComment);
+
+        assertThat(commentRepository.findById(returnedComment.getId()))
+                .isPresent()
+                .get()
+                .isEqualTo(returnedComment);
+    }
+
+    @DisplayName("должен удалять комментарий по id ")
+    @Test
+    void shouldDeleteComment() {
+        assertThat(commentRepository.findById(1L)).isPresent();
+        commentRepository.deleteById(1L);
+        assertThat(commentRepository.findById(1L)).isEmpty();
+    }
+
+    private static List<Author> getDbAuthors() {
+        return IntStream.range(1, 4).boxed()
+                .map(id -> new Author(id, "Author_" + id))
+                .toList();
+    }
+
+    private static List<Genre> getDbGenres() {
+        return IntStream.range(1, 4).boxed()
+                .map(id -> new Genre(id, "Genre_" + id))
+                .toList();
+    }
+
+    private static List<Book> getDbBooks(List<Author> dbAuthors, List<Genre> dbGenres) {
+        return IntStream.range(1, 4).boxed()
+                .map(id -> new Book(id, "BookTitle_" + id, dbAuthors.get(id - 1), dbGenres.get(id - 1)))
+                .toList();
+    }
+
+    private static List<Comment> getDbComments(List<Book> dbBooks) {
+        return IntStream.range(1, 4).boxed()
+                .map(id -> new Comment(id, "Comment_" + id, dbBooks.get(id - 1)))
+                .toList();
+    }
+
+    private static List<Book> getDbBooks() {
+        var dbAuthors = getDbAuthors();
+        var dbGenres = getDbGenres();
+        return getDbBooks(dbAuthors, dbGenres);
+    }
+
+    private static List<Comment> getDbComments() {
+        var dbBooks = getDbBooks();
+        return getDbComments(dbBooks);
+    }
+}
